7fba40a89163e6c73a035ac62ea7d74f3c02de0b
FRAN <fraanco.st@gmail.com>
2019-06-19 19:41:58 -0300

    cambios

--- bfs.py
+++ bfs.py
@@ -10,7 +10,7 @@ import Arbol
 
 #metodo que realiza el recorrido bfs y retorna un conjunto con el/los arboles resultantes
 def bfs_completo(matriz,nodos):
-    blancos = [False] * len(nodos)      #marcamos todos los nodos como blancos
+    blancos = [False] * len(nodos)      #marcamos todos los nodos como blancos O(n)
     foresta = []                        #inicializamos un arreglo para almacenar la foresta
     for u in nodos:                     #por cada nodo O(n)
         if(blancos[u] == False):            #si no esta marcado como blanco
@@ -23,13 +23,14 @@ def bfs(u, matriz, blancos):
     cola = queue.Queue()                #creamos una cola
     blancos[u] = True                   #marcamos el nodo seleccionado como blanco
     cola.put(u)                         #lo ingresamos a la cola
-    pertenecientes = []                 #creamos una lista para ir almacenando nodos recorridos a partir del inicial(arbol resultante)
+    pertenecientes = []                 #creamos una lista para ir almacenando nodos 
+                                        #recorridos a partir del inicial(arbol resultante)
     
-    while (not cola.empty()):           #mientras la cola no este vacia
+    while (not cola.empty()):               #mientras la cola no este vacia                               O(n)
         actual = cola.get()                 #obtenemos el siguiente de la cola
         pertenecientes.append(actual)       #agregamos el nodo actual a pertenecientes
         if(actual in matriz):               #si el actual esta en la matriz
-            for v in matriz.get(actual):    #por cada nodo adyacente al actual
+            for v in matriz.get(actual):    #por cada nodo adyacente al actual (es decir por cada arco)   O(a)
                 if(blancos[v]==False):      #si el nodo adyacente no es blanco
                     blancos[v] = True       #lo marcamos como blanco
                     cola.put(v)             #lo añadimos a la cola
