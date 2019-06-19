88c04a4bf765275f79088ccc9e672fd22bd75902
Andres Garcia <garcia.andi90@gmail.com>
2019-06-19 17:23:17 -0300

    Fix Tabla 2

--- Tabla.py
+++ Tabla.py
@@ -2,7 +2,7 @@
 #esta clase se implemento para generar una tabla de visualizacion de datos de manera ordenada.
 class Tabla:
     def __init__(self):
-        self.head = ["n° Grafo", "Nodos", "Arcos  ", "    Flag-conexo  ", "       BFS     ", "  Disjoin-set   "]
+        self.head = ["n° Grafo", "Nodos", " Arcos  ", "    Flag-conexo  ", "       BFS     ", "  Disjoin-set   "]
         self.head_2 = ["n° Grafo", "Nodos", " Arcos ", "Flag-conexo", "   Kruskal Arcos  ", "  Kruskal MinHeap  ", "  Kruskal c/h   ","  Kruskal s/h   "]
         self.linea = 84
         self.linea_2 = 122
@@ -26,7 +26,7 @@ class Tabla:
         print(" ","-"*self.linea)
 
         for d in self.datos:
-            print("| "," "*(4-len("  ")),d[0]," "*(3-len(str(d[0]))),"| ", d[1], " "*(3-len(str(d[1]))), "| ", d[2], " "*(3-len(str(d[2]))), "|    ", d[3], " "*(6-len(str(d[3]))), "|  ", d[4], " "*(7-len(str(d[4]))), "|  ", d[5], " "*(7-len(str(d[5]))), "| " )
+            print("| "," "*(4-len("  ")),d[0]," "*(3-len(str(d[0]))),"| ", d[1], " "*(3-len(str(d[1]))), "| ", d[2], " "*(6-len(str(d[2]))), "|       ", d[3], " "*(9-len(str(d[3]))), "|  ", d[4], " "*(12-len(str(d[4]))), "|  ", d[5], " "*(13-len(str(d[5]))), "| " )
         print(" ","-"*self.linea)
         print("")
         print("")
@@ -61,7 +61,7 @@ class Tabla:
 
 # datos_2 = [[0,10,20,"si",0.11123,0.121345,0.123456,0.123456], [1,100,200,"si",123.11123,213.121345,123.123456,123.123456]]
 
-# t = Tabla()
+t = Tabla()
 # t.insertar_tabla1(0,10,20,'si',True,True)
 # t.insertar_tabla1(1,100,200,'si',False,False)
 # t.insertar_tabla2(0,10,20,'si',0.11123,0.121345,0.123456,0.123456)
@@ -93,4 +93,14 @@ class Tabla:
 
 
 # t.imprimir_tabla2()
+# t.imprimir_tabla1()
+
+
+
+# t.insertar_tabla1(     1    ,  10   ,  20   ,   "sí"    , 0.0010087490  ,   0.0010087490  ) 
+# t.insertar_tabla1(     2    ,  150  ,  1000 ,   "sí"    ,  0.0010087490  ,   0.1585950851  ) 
+# t.insertar_tabla1(     3    ,  200  ,  3000 ,   "sí"    ,  0.0019953251  ,   0.6074497700  ) 
+# t.insertar_tabla1(     4    ,  300  ,  10000,   "sí"    ,   0.0039892197  ,   3.1835587025  ) 
+# t.insertar_tabla1(     5    ,  400  ,  1000 ,   "no"    ,  0.0030226707  ,   0.5046510696  ) 
+# t.insertar_tabla1(     6    ,  500  ,  10000,   "sí"    ,   0.0050160885  ,   6.0090534687  )
 # t.imprimir_tabla1()
